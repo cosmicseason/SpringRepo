fc20b4fb06fd461a426a13bd7a02f39ecd19844a
Joo <eyebabyuniverse@gmail.com>
2019-06-03 16:09:06 +0900

    lab1 first commit

--- SpringGitTest/src/main/java/com/gura/springtest/MyController.java
+++ SpringGitTest/src/main/java/com/gura/springtest/MyController.java
@@ -1,9 +1,11 @@
 package com.gura.springtest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
-
+ @Autowired
+ public int Dao;
 	
 }
