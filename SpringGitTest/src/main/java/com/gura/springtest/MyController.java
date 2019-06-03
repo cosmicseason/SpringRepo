33de58dc5ccf40083317f7248b55024ecf9e68d3
Joo <eyebabyuniverse@gmail.com>
2019-06-03 16:32:11 +0900

    form MyHome first commit to lab1

--- SpringGitTest/src/main/java/com/gura/springtest/MyController.java
+++ SpringGitTest/src/main/java/com/gura/springtest/MyController.java
@@ -3,9 +3,18 @@ package com.gura.springtest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
+
 @Controller
 public class MyController {
- @Autowired
- public int Dao;
+ 
+@Autowired
+public int num;
+ 
+ public String InsertGoods() {
+ 
+	 	String nS=Integer.toString(num);
+	 	
+	 	return nS;
 	
+	}
 }
